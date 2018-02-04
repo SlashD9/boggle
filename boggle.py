0cb8f70557ca2b52da1f6d27f329046c63218302
Colin Daly <colin.p.s.daly@gmail.com>
2018-02-04 21:18:36 +0000

    Changed the do_search to use word_in_dictionary function to reduce the time it takes to run

--- boggle.py
+++ boggle.py
@@ -72,6 +72,9 @@ def path_to_word(grid, path):
     """
     return ''.join([grid[p] for p in path])
     
+def word_in_dictionary(word, dict):
+    return word in dict
+    
 def search(grid, dictionary):
     """
     Search through the paths to locate words by matching
@@ -82,7 +85,7 @@ def search(grid, dictionary):
     
     def do_search(path):
         word = path_to_word(grid, path)
-        if word in dictionary:
+        if word_in_dictionary(word, dictionary):
             paths.append(path)
         for next_pos in neighbours[path[-1]]:
             if next_pos not in path:
