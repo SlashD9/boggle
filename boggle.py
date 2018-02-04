6c9334a35e565f40a8d12c91da0fa86c2d173119
Colin Daly <colin.p.s.daly@gmail.com>
2018-02-04 15:59:04 +0000

    added some more comments in the boggle file

--- boggle.py
+++ boggle.py
@@ -4,7 +4,10 @@ from random import choice
 def make_grid(width, height):
     """
     Creates a grid that will hold all of the tiles
-    for a boggle game
+    for a boggle game, with letters inside each of the coordinates.
+    
+    We use the random - choice and send it ascii_uppercase to generate letters
+    for each of the grid coordinates
     """
     return {(row, col): choice(ascii_uppercase) 
         for row in range(height)
